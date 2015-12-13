eb4a194b665eab382dcd02151148b3806a20581b
wyf7107 <wyf7107@gmail.com>
2015-12-13 12:27:39 -0500

    Initial Commit

---
+++ Invite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Invite
+//
+//  Created by Yifan Wang on 15/12/13.
+//  Copyright © 2015年 Yifan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Invite/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Invite
+//
+//  Created by Yifan Wang on 15/12/13.
+//  Copyright © 2015年 Yifan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
